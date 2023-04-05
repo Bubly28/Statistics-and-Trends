0fb075d004a319a78ecd688a8c56fabe4ee2ce30
Bubly28 <125889921+Bubly28@users.noreply.github.com>
2023-04-05 13:42:53 +0100

    Add files via upload
    Added code for heatmap

--- Statistics and Trends_Code.py
+++ Statistics and Trends_Code.py
@@ -8,6 +8,7 @@ Created on Tue Apr  4 20:15:02 2023
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def data_reading(file):
@@ -64,6 +65,45 @@ def filter_data(data_r, col_name,  col_val, contries, years):
     return data_filter, data_filter_trans
 
 
+def stat_fn(s_df, col_s, val_s, yr_s, i_s):
+    '''
+    This function used to filter data for heatmap and statistical tools
+
+    Parameters
+    ----------
+    s_df : Dataset
+    col_s : Name of the required column
+    val_s : Value in the chosen column
+    yr_s : Years for filtering data
+    i_s : Indicators for filtering data
+
+    Returns
+    -------
+
+    df_stat : Filtered dataset
+
+    '''
+
+    # Grouping the dataset by a column name
+    df_stat = s_df.groupby(col_s, group_keys=True)
+    # Taking the dataset of a particular column value
+    df_stat = df_stat.get_group(val_s)
+    # Resetting the index of the grouped dataset
+    df_stat = df_stat.reset_index()
+    # Setting Indicator Name as the index
+    df_stat.set_index('Indicator Name', inplace=True)
+    # Cropping the data by years
+    df_stat = df_stat.loc[:, yr_s]
+    # Transposing the filtered dataset
+    df_stat = df_stat.transpose()
+    # Cropping the data by indicators
+    df_stat = df_stat.loc[:, i_s]
+    # Dropping the nan values
+    df_stat = df_stat.dropna(axis=1)
+    # Return statement is used at the end of a function
+    return df_stat
+
+
 def bar_plot(f_data_b, title_name_b, x_lab_b, y_lab_b):
     '''
     This function is used for bar plotting
@@ -78,9 +118,9 @@ def bar_plot(f_data_b, title_name_b, x_lab_b, y_lab_b):
     '''
 
     bx = f_data_b.plot.bar(x='Country Name', rot=0,
-                         figsize=(50, 30), fontsize=50)
+                           figsize=(50, 30), fontsize=50)
     # Setting yticks
-    bx.set_yticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11])
+    bx.set_yticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
     # Setting the title for the plot
     bx.set_title(title_name_b.upper(), fontsize=60, fontweight='bold')
     # Setting the x-label for the plot
@@ -104,16 +144,16 @@ def line_plot(f_data_l, title_name_l, x_lab_l, y_lab_l):
     title_name_l : Title of the line plot
     x_lab_l : x-label
     y_lab_l : y-label
-    
+
     '''
-    
-    lx = f_data_l.plot.line(figsize=(50,30), fontsize=36, linewidth=7.0)
+
+    lx = f_data_l.plot.line(figsize=(50, 30), fontsize=36, linewidth=7.0)
     # Setting yticks
     lx.set_yticks([50, 55, 60, 65, 70, 75, 80, 85, 90])
     # Setting the title for the plot
     lx.set_title(title_name_l.upper(), fontsize=60, fontweight='bold')
     # Setting the x-label for the plot
-    lx.set_xlabel(x_lab_l,fontsize=50)
+    lx.set_xlabel(x_lab_l, fontsize=50)
     # Setting the y-label for the plot
     lx.set_ylabel(y_lab_l, fontsize=50)
     lx.legend(fontsize=50)
@@ -121,6 +161,32 @@ def line_plot(f_data_l, title_name_l, x_lab_l, y_lab_l):
     plt.show()
     # Return statement is used at the end of a function
     return
+
+
+def heat_map(data_h, con_h, gen, color):
+    '''
+    This function is used to plot heatmap
+
+    Parameters
+    ----------
+    data_h : dataset filtered for plotting heatmap
+    con_h : Country name
+    gen : Gender 
+    color : Color of the heatmap
+
+    Returns
+    -------
+    data_h : The dataset used for plotting heatmap
+
+    '''
+    plt.figure(figsize=(20, 18))
+    h_map = sns.heatmap(data_h.corr(), annot=True, cmap=color)
+    #Setting title for heatmap
+    h_map.set_title(con_h + '(' + gen + ')')
+    plt.savefig(con_h + gen + ".png", dpi=300, bbox_inches='tight')
+    # Return statement is used at the end of a function
+    return data_h
+
 
 # Calling the function for reading data and assigning it in a variable
 dataset = data_reading("Gender_Equality.csv")
@@ -154,25 +220,48 @@ bar_plot(data_bar2, 'Unemployment (female)',
 
 
 # List of countries for line plotting
-countries_line= ['Japan', 'Iraq', 'Kuwait', 'Afghanistan', 'Philippines']
+countries_line = ['Japan', 'Iraq', 'Kuwait', 'Afghanistan', 'Philippines']
 # List of years for line plotting
-year_line = ['1995', '1998', '2001', '2004', '2007', '2010', '2013', '2016', '2019']
+year_line = ['1995', '1998', '2001', '2004',
+             '2007', '2010', '2013', '2016', '2019']
 # Calling the data filtering function for 1st line plot and assinging its values in variables
-data_line1, data_line1_t = filter_data(dataset, 'Indicator Name','Life expectancy at birth, male (years)', countries_line, year_line)
+data_line1, data_line1_t = filter_data(
+    dataset, 'Indicator Name', 'Life expectancy at birth, male (years)', countries_line, year_line)
 # Printing the dataset with years as columns
 print(data_line1)
 # Printing the transposed dataset with countries as columns
 print(data_line1_t)
 # Calling the function for 2nd line plotting
-line_plot(data_line1_t, 'Life expectancy at birth (male)', 'Year', 'Life expectancy at birth, male (years)')
+line_plot(data_line1_t, 'Life expectancy at birth (male)',
+          'Year', 'Life expectancy at birth, male (years)')
 
 
 # Calling the data filtering function for 2nd line plot and assinging its values in variables
-data_line2, data_line2_t = filter_data(dataset, 'Indicator Name','Life expectancy at birth, female (years)', countries_line, year_line)
+data_line2, data_line2_t = filter_data(
+    dataset, 'Indicator Name', 'Life expectancy at birth, female (years)', countries_line, year_line)
 # Printing the dataset with years as columns
 print(data_line2)
 # Printing the transposed dataset with countries as columns
 print(data_line2_t)
 # Calling the function for 2nd line plotting
-line_plot(data_line2_t, 'Life expectancy at birth (female)', 'Year', 'Life expectancy at birth, female (years)')
+line_plot(data_line2_t, 'Life expectancy at birth (female)',
+          'Year', 'Life expectancy at birth, female (years)')
 
+# List of years for plotting heatmap
+years_h = ['2000', '2002', '2004', '2006', '2008', '2010']
+
+# List of indicators for plotting 1st heatmap
+ind_h_m = ['Mortality rate, adult, male (per 1,000 male adults)', 'Survival to age 65, male (% of cohort)', 'Unemployment, male (% of male labor force) (modeled ILO estimate)', 'School enrollment, tertiary, male (% gross)',
+           'Employment in services, male (% of male employment) (modeled ILO estimate)', 'Wage and salaried workers, male (% of male employment) (modeled ILO estimate)', 'Self-employed, male (% of male employment) (modeled ILO estimate)']
+# Calling the stat function
+d_heat = stat_fn(dataset, 'Country Name', 'India', years_h, ind_h_m)
+# Calling the function for plotting the 1st heatmap
+heat_map(d_heat, 'India', 'Male', 'RdPu')
+
+# List of indicators for plotting 2nd heatmap
+ind_h_f = ['Mortality rate, adult, female (per 1,000 female adults)', 'Survival to age 65, female (% of cohort)', 'Unemployment, female (% of female labor force) (modeled ILO estimate)', 'School enrollment, tertiary, female (% gross)',
+           'Employment in services, female (% of female employment) (modeled ILO estimate)', 'Wage and salaried workers, female (% of female employment) (modeled ILO estimate)', 'Self-employed, female (% of female employment) (modeled ILO estimate)']
+# Calling the stat function
+d_heat = stat_fn(dataset, 'Country Name', 'India', years_h, ind_h_f)
+# Calling the function for plotting the 2nd heatmap
+heat_map(d_heat, 'India', 'Female', 'GnBu')
